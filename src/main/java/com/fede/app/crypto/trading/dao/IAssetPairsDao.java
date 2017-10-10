1a6a27f9a4d3965b74f0c84e0134cbdf53b90cd0
joker1618 <federicobarbano@gmail.com>
2017-10-10 19:28:52 +0200

    asset pairs dao OK

---
+++ src/main/java/com/fede/app/crypto/trading/dao/IAssetPairsDao.java
@@ -0,0 +1,15 @@
+package com.fede.app.crypto.trading.dao;
+
+import com.fede.app.crypto.trading.model._public.AssetPair;
+
+import java.util.List;
+
+/**
+ * Created by f.barbano on 30/09/2017.
+ */
+public interface IAssetPairsDao {
+
+	List<AssetPair> getAssetPairs();
+	void persistNewAssetPairs(Long callTime, List<AssetPair> newAssetPairs);
+
+}
